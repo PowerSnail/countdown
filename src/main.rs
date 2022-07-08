d8da3fe5f59aaf57a6d3f096c14af8a1cae13629
PowerSnail <hj@powersnail.com>
2022-07-08 11:24:25 -0700

    Update dependencies to clap 3

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,20 @@
-use clap::Clap;
+use clap::Parser;
 use notify_rust::error::Result;
 use notify_rust::Notification;
 use std::time::Duration;
 
-#[derive(Clap)]
-#[clap(
-    version = "1.0.2",
-    author = "PowerSnail <hj@powersnail.com>",
-    about = "Send a system-wide notification after a countdown"
-)]
+#[derive(Parser, Debug)]
+#[clap(version, author, about)]
 struct Opts {
-    #[clap(
-        about = "A float number for the countdown, unit being second, unless --minute is specified."
-    )]
+    /// A float number for the countdown, unit being second, unless --minute is specified.
     time: f32,
 
-    #[clap(
-        default_value = "Timer",
-        about = "A short message to show in the notification."
-    )]
+    /// A short message to show in the notification.
+    #[clap(default_value = "Timer")]
     text: String,
 
-    #[clap(
-        short,
-        long,
-        about = "Change the unit of time to minute, instead of second."
-    )]
+    /// Change the unit of time to minute, instead of second.
+    #[clap(short, long)]
     minute: bool,
 }
 
@@ -38,6 +27,7 @@ fn main() -> Result<()> {
     };
     std::thread::sleep(Duration::from_secs_f32(opts.time * time_unit));
     Notification::new()
+        .appname("Snail Countdown")
         .summary("Time's up")
         .body(&opts.text)
         .icon("clock")
